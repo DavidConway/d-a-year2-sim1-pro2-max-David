f95498f343e81bcd81fcea0b59165cc1894048f5
Max <Max@DESKTOP-102TDJ3.Home>
2018-11-20 18:36:42 +0000

    Added new fxml and controller class/ configured main

---
+++ d&a-year2-sim1-pro2-max-David/src/application/Controller.java
@@ -0,0 +1,50 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+	@FXML
+	private TextField textTitle;
+
+	@FXML
+	private TextField textAuthor;
+
+	@FXML
+	private TextField textYear;
+
+	@FXML
+	private TextField textPages;
+
+	@FXML
+	private TextField textGenre;
+
+	@FXML
+	private TextArea textPlot;
+
+	@FXML
+	private TextField textName;
+
+	@FXML
+	private TextField textGender;
+
+	@FXML
+	private TextArea textDescription;
+
+	@FXML
+	void addBookOnClick(ActionEvent event) {
+
+	}
+
+	@FXML
+	void addCharOnClick(ActionEvent event) {
+
+	}
+
+}
+
+
+
