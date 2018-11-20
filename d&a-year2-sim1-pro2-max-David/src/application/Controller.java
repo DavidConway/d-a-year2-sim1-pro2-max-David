13717c6bb424ca7283d21da5d58adccf444d8a50
Max <Max@DESKTOP-102TDJ3.Home>
2018-11-20 18:52:01 +0000

    Some comments

--- d&a-year2-sim1-pro2-max-David/src/application/Controller.java
+++ d&a-year2-sim1-pro2-max-David/src/application/Controller.java
@@ -6,31 +6,26 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class Controller {
-
+	
+	//FIELDS FOR ADDING A BOOK
 	@FXML
 	private TextField textTitle;
-
 	@FXML
 	private TextField textAuthor;
-
 	@FXML
 	private TextField textYear;
-
 	@FXML
 	private TextField textPages;
-
 	@FXML
 	private TextField textGenre;
-
 	@FXML
 	private TextArea textPlot;
-
+	
+	//FIELDS FOR ADDING A CHARACTER TO LIST
 	@FXML
 	private TextField textName;
-
 	@FXML
 	private TextField textGender;
-
 	@FXML
 	private TextArea textDescription;
 
