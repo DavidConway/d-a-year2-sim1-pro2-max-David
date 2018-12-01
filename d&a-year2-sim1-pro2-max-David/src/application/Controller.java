d6675c2e5fc7cab1faa9401d1e71f434c8d267ff
Max Larkin <maxarastir@gmail.com>
2018-12-01 15:34:02 +0000

    Updating GUI appearance

--- d&a-year2-sim1-pro2-max-David/src/application/Controller.java
+++ d&a-year2-sim1-pro2-max-David/src/application/Controller.java
@@ -10,6 +10,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Screen;
 
 public class Controller {
 
@@ -106,6 +107,12 @@ public class Controller {
     @FXML
     private Button pagesButton;
    
+    @FXML
+    void initialize()
+    {
+    	bookScrollPane.setMinViewportHeight(Screen.getPrimary().getBounds().getHeight());
+    	bookScrollPane.setFitToHeight(true);
+    }
     //METHODS FOR ADD/REMOVING/EDITING
     
     @FXML
