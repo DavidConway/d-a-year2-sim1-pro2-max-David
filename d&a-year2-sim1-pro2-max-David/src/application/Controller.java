270a2cae99da9dbe917378e2ef4f366a377a4387
Max Larkin <maxarastir@gmail.com>
2019-01-02 20:10:11 +0000

    Updating gui

--- d&a-year2-sim1-pro2-max-David/src/application/Controller.java
+++ d&a-year2-sim1-pro2-max-David/src/application/Controller.java
@@ -7,9 +7,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
-import com.sun.xml.internal.messaging.saaj.soap.StringDataContentHandler;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
